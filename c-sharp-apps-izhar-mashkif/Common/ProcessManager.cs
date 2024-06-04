09a9ecd9c53fcbe5abb448d75377c0d233b3dd61
lingar <yimprogramming@gmail.com>
2024-06-04 15:15:05 +0300

    Finish taask

--- c-sharp-apps-izhar-mashkif/Common/ProcessManager.cs
+++ c-sharp-apps-izhar-mashkif/Common/ProcessManager.cs
@@ -14,38 +14,39 @@ namespace c_sharp_apps_izhar_mashkif.Common
     {
         public static void RunMainProcess ()
         {
-            Console.WriteLine("Choose one options: ");
-            Console.WriteLine("1 – Bank App | 2 – Sport App | 3 – Transportation App | 4 – Draft App | 0- Exit");
-
-            int choose = int.Parse(Console.ReadLine());
-
-            while (choose != 0)
+            int choose = 0;
+            do
             {
-                switch (choose)
-                {
-                    case 0:
-                        Console.WriteLine("App Exit");
-                        return;
-                    case 1:
-                        BankAppMain.MainEntry();
-                        break;
-                    case 2:
-                        SportAppMain.MainEntry();
-                        break;
-                    case 3:
-                        TransportationAppMain.MainEntry();
-                        break;
-                    case 4:
-                        DraftAppMain.MainEntry();
-                        break;
-                }
-
                 Console.WriteLine("Choose one options: ");
                 Console.WriteLine("1 – Bank App | 2 – Sport App | 3 – Transportation App | 4 – Draft App | 0- Exit");
 
                 choose = int.Parse(Console.ReadLine());
+                {
+                    switch (choose)
+                    {
+                        case 0:
+                            Console.WriteLine("App Exit");
+                            return;
+                        case 1:
+                            BankAppMain.MainEntry();
+                            break;
+                        case 2:
+                            SportAppMain.MainEntry();
+                            break;
+                        case 3:
+                            TransportationAppMain.MainEntry();
+                            break;
+                        case 4:
+                            DraftAppMain.MainEntry();
+                            break;
+                    }
 
+                   
+
+                }
             }
+            while (choose != 0);
+            
         }
     }
 }
