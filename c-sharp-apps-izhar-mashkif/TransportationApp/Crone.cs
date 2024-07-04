7ba953f2f7410eeffda9f7851c52e23f8b29d668
lingar <yimprogramming@gmail.com>
2024-07-05 00:53:53 +0300

    Task 3 skeleton

---
+++ c-sharp-apps-izhar-mashkif/TransportationApp/Crone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_izhar_mashkif.TransportationApp
+{
+    public class Crone
+    {
+        private readonly int rows, columns;
+
+
+    }
+}
---
+++ c-sharp-apps-izhar-mashkif/TransportationApp/PassengersAirplain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_apps_izhar_mashkif.TransportationApp
+{
+    public class PassengersAirplain 
+    {
+        private int enginesNum;
+        private int wingLength;
+        private int rows; 
+        private int columns;
+
+    }
+}
